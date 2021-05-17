8c90283b9f354d525b67c59131549c6c6915622b
Denys <sagottw@gmail.com>
2021-05-17 16:30:51 +0300

    Finished site (Need to change few imgs etc.)

--- modal.js
+++ modal.js
@@ -1,18 +1,23 @@
 let modalWindow = document.querySelector(".modal");
 let cross = document.querySelector(".cross");
 let blogDescription = document.querySelectorAll(".blog-description");
+let blogBtn = document.querySelectorAll(".blog-btn");
+let body = document.querySelector("body")
 
-console.log(modalWindow);
-console.log(cross);
-console.log(blogDescription);
-
-
+for (let i=0; i < blogBtn.length; i++) {
+    blogBtn[i].addEventListener("click", function(){
+        modalWindow.classList.add("dis-block")
+        body.classList.add("bgc-dark-js")
+    })
+}
 for (let i=0; i < blogDescription.length; i++) {
     blogDescription[i].addEventListener("click", function(){
         modalWindow.classList.add("dis-block")
+        body.classList.add("bgc-dark-js")
     })
 }
 
 cross.addEventListener("click", function(){
     modalWindow.classList.remove("dis-block")
+    body.classList.remove("bgc-dark-js")
 })
